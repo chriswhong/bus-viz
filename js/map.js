061b1ff49d77ab0379dab1810e560bc8f3d91271
Chris Whong <chris.m.whong@gmail.com>
2020-01-30 09:52:19 -0500

    clean up and comment

--- js/map.js
+++ js/map.js
@@ -4,25 +4,15 @@ const map = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/light-v9', //hosted style id
   center: [-73.95, 40.68], // starting position
   zoom: 12, // starting zoom
-  // hash: true,
+  hash: true
 });
-
-const minuteRate = 1000
-
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
 
 // add the zoom/rotate/pitch control to the map
 map.addControl(new mapboxgl.NavigationControl());
 
 map.on('style.load', () => {
 
+  // add static layers (bus route linestrings)
   map.addSource('b67-route-northbound', {
     type: 'geojson',
     data: 'data/b67-route-northbound.geojson'
@@ -63,18 +53,23 @@ map.on('style.load', () => {
 const asJson = (csv) => {
   const headers = csv.shift()
 
-  const json = csv.map((row) => {
+  return csv.map((row) => {
     const object = {}
     headers.forEach((header, i) => {
       object[header] = row[i]
     })
     return object
   })
-
-  return json
 }
 
+
+
+// renderVehicles will animate vehicles between their current position and their next position
+// once done, it calls itself until reaching the end of the data
 const renderVehicles = (timestamps, data, timestampCounter) => {
+  // how many milliseconds === 1 minute in real life
+  const minuteRate = 5000
+
   const currentTimestamp = timestamps[timestampCounter]
   const currentMoment = moment.unix(currentTimestamp)
   // get rows for this timestamp
@@ -132,7 +127,7 @@ const renderVehicles = (timestamps, data, timestampCounter) => {
       })
     }
 
-    // interpolate position along string
+    // determine position along string
     const startPosition = [observation.longitude, observation.latitude]
     const nextObservation = observationsForNextTimestamp.find(d => d.vehicleRef === observation.vehicleRef)
     if (nextObservation) {
@@ -146,9 +141,12 @@ const renderVehicles = (timestamps, data, timestampCounter) => {
       const lineBetweenPoints = turf.lineString([startPosition, endPosition])
       const incrementLength = turf.length(lineBetweenPoints) / frames
 
+      // create an interval to update the data {frames} times
+      // we already rendered the initial position, so kick things off at 1, not 0
       let counter = 1
       const dataInterval = setInterval(() => {
         updateData(lineBetweenPoints, incrementLength, counter, frames, observation.vehicleRef)
+        // the end position is just the 0 position of the next timestamp, so we are iterating over n-2
         if (counter === frames - 1) {
           clearInterval(dataInterval)
         } else {
@@ -169,14 +167,15 @@ const renderVehicles = (timestamps, data, timestampCounter) => {
     map.getSource(sourceId).setData(pointAlong);
   }
 
-  // remove orphaned sources
+  // remove orphaned layers and sources
   sourcesOnMap.forEach((id) => {
     const style = map.getStyle()
     if (style.layers.find(d => d.id === `${id}-circle`)) map.removeLayer(`${id}-circle`)
-
     if (style.sources[id]) map.removeSource(id)
   })
 
+  // move to the next frame after {minuteRate}, which should be the same amount
+  // of time needed to animate {frames} frames
   if (timestampCounter < timestamps.length - 2 ) {
     setTimeout(() => {
       timestampCounter += 1
@@ -186,8 +185,6 @@ const renderVehicles = (timestamps, data, timestampCounter) => {
 }
 
 const startAnimation = (data) => {
-  // // filter for dev only
-  // data = data.filter(d => d.vehicleRef === 'MTA NYCT_381')
   // get all of the unique timestamps
   const timestamps = data
     .map(d => d.timestamp)
@@ -198,5 +195,4 @@ const startAnimation = (data) => {
   let currentTimestamp = timestamps[timestampCounter]
 
   renderVehicles(timestamps, data, timestampCounter)
-
 }
